ddbe4b93d662ff63ca7d43e6f071d9f32888bbe6
sjdps <sjdps001@gmail.com>
2018-07-26 18:48:41 -0500

    CRUD en modelos Propietario y Vehiculo

--- lib/prueba_Liftit/propietario.ex
+++ lib/prueba_Liftit/propietario.ex
@@ -1,7 +1,8 @@
 defmodule Prueba_Liftit.Propietario do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Prueba_Liftit.Repo
+  alias Prueba_Liftit.Propietario
 
   schema "propietarios" do
     field :cedula, :string
@@ -18,4 +19,97 @@ defmodule Prueba_Liftit.Propietario do
     |> validate_required([:nombre, :cedula])
     |> unique_constraint(:cedula)
   end
+
+  @doc """
+  Creates a propietario.
+
+  ## Examples
+
+      iex> create_propietario(%{field: value})
+      {:ok, %Propietario{}}
+
+      iex> create_propietario(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_propietario(attrs \\ %{}) do
+    %Propietario{}
+    |> Propietario.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  # def create_propietario(%Propietario{} = propietario, attrs) do
+  #   propietario
+  #   |> changeset(attrs)
+  #   |> Repo.insert
+  # end
+
+  def list_propietarios do
+    Repo.all(Propietario)
+  end
+
+  @doc """
+  Gets a single propietario.
+
+  Raises `Ecto.NoResultsError` if the Propietario does not exist.
+
+  ## Examples
+
+      iex> get_propietario!(123)
+      %Propietario{}
+
+      iex> get_propietario!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_propietario!(id), do: Repo.get!(Propietario, id)
+
+
+  @doc """
+  Updates a propietario.
+
+  ## Examples
+
+      iex> update_propietario(propietario, %{field: new_value})
+      {:ok, %Propietario{}}
+
+      iex> update_propietario(propietario, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_propietario(%Propietario{} = propietario, attrs) do
+    propietario
+    |> Propietario.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Propietario.
+
+  ## Examples
+
+      iex> delete_propietario(propietario)
+      {:ok, %Propietario{}}
+
+      iex> delete_propietario(propietario)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_propietario(%Propietario{} = propietario) do
+    Repo.delete(propietario)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking propietario changes.
+
+  ## Examples
+
+      iex> change_propietario(propietario)
+      %Ecto.Changeset{source: %Propietario{}}
+
+  """
+  def change_propietario(%Propietario{} = propietario) do
+    Propietario.changeset(propietario, %{})
+  end
+
 end
--- lib/prueba_Liftit/vehiculo.ex
+++ lib/prueba_Liftit/vehiculo.ex
@@ -1,14 +1,14 @@
 defmodule Prueba_Liftit.Vehiculo do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Prueba_Liftit.Repo
+  alias Prueba_Liftit.Vehiculo
 
   schema "vehiculos" do
     field :marca, :string
     field :placa, :string
     field :tipo, :string
     belongs_to :propietario, Prueba_Liftit.Propietario
-
 
     timestamps()
   end
@@ -19,5 +19,85 @@ defmodule Prueba_Liftit.Vehiculo do
     |> cast(attrs, [:placa, :marca, :tipo])
     |> validate_required([:placa, :marca, :tipo])
     |> unique_constraint(:placa)
+  end
+
+  def create_vehiculo(attrs \\ %{}) do
+    %Vehiculo{}
+    |> Vehiculo.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  # def create_vehiculo(%Vehiculo{} = vehiculo, attrs) do
+  #   vehiculo
+  #   |> changeset(attrs)
+  #   |> Repo.insert
+  # end
+
+  def list_vehiculos do
+    Repo.all(Vehiculo)
+  end
+
+  @doc """
+  Gets a single vehiculo.
+
+  Raises `Ecto.NoResultsError` if the Vehiculo does not exist.
+
+  ## Examples
+
+      iex> get_vehiculo!(123)
+      %Vehiculo{}
+
+      iex> get_vehiculo!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_vehiculo!(id), do: Repo.get!(Propietario, id)
+
+
+  @doc """
+  Updates a vehiculo.
+
+  ## Examples
+
+      iex> update_vehiculo(vehiculo, %{field: new_value})
+      {:ok, %Vehiculo{}}
+
+      iex> update_vehiculo(vehiculo, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_vehiculo(%Vehiculo{} = vehiculo, attrs) do
+    vehiculo
+    |> Vehiculo.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Vehiculo.
+
+  ## Examples
+
+      iex> delete_vehiculo(vehiculo)
+      {:ok, %Vehiculo{}}
+
+      iex> delete_vehiculo(vehiculo)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_vehiculo(%Vehiculo{} = vehiculo) do
+    Repo.delete(vehiculo)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking vehiculo changes.
+
+  ## Examples
+
+      iex> change_vehiculo(vehiculo)
+      %Ecto.Changeset{source: %Vehiculo{}}
+
+  """
+  def change_vehiculo(%Vehiculo{} = vehiculo) do
+    Vehiculo.changeset(vehiculo, %{})
   end
 end
